0bc04371c408b789f3444333653b7d0ca206c31f
Nekruz Rahimov <RahimovN@HUMO.local>
2021-02-20 17:13:09 +0500

    changed(news.expire_at type string -> int)

--- models/news.go
+++ models/news.go
@@ -4,7 +4,7 @@ type News struct {
 	Id int `json:"id"`
 	Title string `json:"title"`
 	ShortDesc string `json:"short_desc" db:"short_desc"`
-	ExpireAt string `json:"expire_at" db:"expire_at"`
+	ExpireAt int64 `json:"expire_at" db:"expire_at"`
 	Img string `json:"img"`
 	FullDesc string `json:"full_desc" db:"full_desc"`
 	Status bool `json:"status"`
